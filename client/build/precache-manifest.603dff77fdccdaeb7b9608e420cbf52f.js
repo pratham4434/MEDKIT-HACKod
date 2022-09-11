b5ad21af0b0552eb5430e47e9e762bb34e5c046c
Pratham Sharma <91470779+pratham4434@users.noreply.github.com>
2022-09-11 19:48:40 +0530

    build added

---
+++ client/build/precache-manifest.603dff77fdccdaeb7b9608e420cbf52f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5653af5b07c8d1494761",
+    "url": "/static/css/main.7c6b3f88.chunk.css"
+  },
+  {
+    "revision": "5653af5b07c8d1494761",
+    "url": "/static/js/main.5653af5b.chunk.js"
+  },
+  {
+    "revision": "6eb7eb008341f0ed4867",
+    "url": "/static/css/1.6c0b7224.chunk.css"
+  },
+  {
+    "revision": "6eb7eb008341f0ed4867",
+    "url": "/static/js/1.6eb7eb00.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6afe48c5bbe5629d1147b9a51f921906",
+    "url": "/static/media/10130.6afe48c5.jpg"
+  },
+  {
+    "revision": "a608568ddc67e24e64fd641954d6ebb4",
+    "url": "/static/media/hospitalmarker.a608568d.png"
+  },
+  {
+    "revision": "d2f79806cd481a48567e20aa28a46cf1",
+    "url": "/static/media/login.d2f79806.gif"
+  },
+  {
+    "revision": "378b988eb0b23691190225bdc8544dd4",
+    "url": "/static/media/banner-bg2.378b988e.png"
+  },
+  {
+    "revision": "4438bbb86afc1bdf15fe3f8d5124d25a",
+    "url": "/static/media/doc.4438bbb8.png"
+  },
+  {
+    "revision": "7d45d60e709c85758c944d00471816b9",
+    "url": "/static/media/header-img.7d45d60e.png"
+  },
+  {
+    "revision": "a58a4e6ddae2160e80bb40ea04276c2e",
+    "url": "/index.html"
+  }
+];
